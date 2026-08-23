da8fb0439643f3e6b02d59394012370ca74cc77e
tubaikaran2-alt <tubaikaran2@gmail.com>
2026-08-23 13:49:38 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/Daily-Expense/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
